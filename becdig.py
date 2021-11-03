5e7a3cf599638094282e788566398507f8a07379
Maddox Williams <83250335+maddox-w@users.noreply.github.com>
2021-11-03 17:52:45 -0500

    v1.2

--- becdig.py
+++ becdig.py
@@ -6,17 +6,22 @@ import os
 import subprocess
 # Required for retrieving scamalytics data
 import becscan
-# Required for writing retrieved IPs to xlsx
-import xlsxwriter
 # Required for validating .eml first hop
 import ipaddress
 # Required for tracking eml parsing
 from alive_progress import alive_bar
+# Required for writing retrieved IPs to xlsx
+import pandas as pd
+# Required for dumping dict output to file
+import json
 
-#Tracks .eml files unable to be loaded with eml-analyzer
-unloaded_files = []
+returned_dict = {}
 
 def grab_email_ips():
+
+	#Tracks .eml files unable to be loaded with eml-analyzer
+	unloaded_files = []
+	error_count = 0
 	#Grab from eml_files directory
 	print("Grabbing eml files...")
 	eml_files = []
@@ -38,11 +43,16 @@ def grab_email_ips():
 			saved_file_name = eml_files[x].replace(" ", "")
 			#print(f"python3 cli_script.py --header -i \"{cwd}\\eml_files\\{eml_files[x]}\" > \"{cwd}\\parsed_eml\\{saved_file_name}_headers.txt\"")
 			#run([f"python3 cli_script.py --header -i \"{cwd}\\eml_files\\{eml_files[x]}\" > \"{cwd}\\parsed_eml\\{saved_file_name}_headers.txt\""], shell=True)
-			try:
-				subprocess.run(f'python eml_analyzer.py --header -i \"{cwd}\\{path}\\{eml_files[x]}\" > \"{cwd}\\parsed_eml\\{saved_file_name}_headers.txt\"',stderr=subprocess.DEVNULL, shell=True)
+			message = subprocess.run(f'python eml_analyzer.py --header -i \"{cwd}\\{path}\\{eml_files[x]}\" > \"{cwd}\\parsed_eml\\{saved_file_name}_headers.txt\"',stderr=subprocess.DEVNULL, shell=True)
+			complete_test = str(message)[0:16]
 			#os.system(f"python eml_analyzer.py --header -i \"{cwd}\\{path}\\{eml_files[x]}\" > \"{cwd}\\parsed_eml\\{saved_file_name}_headers.txt\"")
-			except:
+			if str(complete_test) != "CompletedProcess":
+				cls = lambda: os.system('cls')
+				cls()
 				unloaded_files.append(saved_file_name)
+				error_count = error_count + 1
+			else:
+				pass
 			bar()
 			x = x + 1
 		print("Done!")
@@ -52,8 +62,8 @@ def grab_email_ips():
 			error_file.write(error + "\n")
 			error_file.close()
 		print("Done!")
-	#Read parsed files into a list
 
+	#Read parsed files into a list
 	parsed_eml_files = []
 	path = 'parsed_eml'
 
@@ -65,50 +75,56 @@ def grab_email_ips():
 
 	print("Reading parsed data, please wait...")
 	#Read parsed header data files into a string, search for value between "Authentication-Results-Original" and ")" - This location is the first hop.
+	
+	#Parsed eml's need to be the key, with the IP as the value.
+
+	parsed_eml_dict = {}
+
 	ip_list = []
 	x = 0
-	for files in eml_files:
+	z = 0
+	with alive_bar(len(eml_files)) as bar:
+		for files in parsed_eml_files:
 	
-		current_file = (f"{cwd}\\parsed_eml\\{parsed_eml_files[x]}")
+			current_file = (f"{cwd}\\parsed_eml\\{parsed_eml_files[x]}")
 	
-		with open(f'{current_file}', 'r') as file:
-			data = file.read()
-			data = re.sub(r"[\n\t\s]*", "", data)
+			with open(f'{current_file}', 'r') as file:
+				data = file.read()
+				data = re.sub(r"[\n\t\s]*", "", data)
 
-		start = 'Authentication-Results-Original'
-		end = 'X-Mimecast-Spam-Score'
-		first_hop_field = data[data.find(start)+len(start):data.rfind(end)]
-		#print(f"{x}: {first_hop_field}")
-		#Grab first hop ip, located between designated and as permitted sender
+			start = 'Authentication-Results-Original'
+			end = 'X-Mimecast-Spam-Score'
+			first_hop_field = data[data.find(start)+len(start):data.rfind(end)]
+			#print(f"{x}: {first_hop_field}")
+			#Grab first hop ip, located between designated and as permitted sender
 
-		start = 'designates'
-		end = 'aspermittedsender'
-		first_hop_ip = first_hop_field[first_hop_field.find(start)+len(start):first_hop_field.rfind(end)]
+			start = 'designates'
+			end = 'aspermittedsender'
+			first_hop_ip = first_hop_field[first_hop_field.find(start)+len(start):first_hop_field.rfind(end)]
 		
-		#Test if IP was found...
-		try:
-			network = ipaddress.IPv4Network(first_hop_ip)
-			ip_list.append(first_hop_ip)
-			#print(first_hop_ip)
-		except ValueError:
-			#invalid IP
-			pass
-		#print(f"File: {parsed_eml_files[x]}, IP:{first_hop_ip}")
-		x = x + 1
-	print(f"Done! {x} addresses found.")
+			#Test if IP was found...
+			try:
+				network = ipaddress.IPv4Network(first_hop_ip)
+				file_name = parsed_eml_files[x]
+				#As IPs get appended to the list, keys are going to be
+				parsed_eml_dict[file_name] = first_hop_ip			
+				#print(first_hop_ip)
+			except ValueError:
+				#invalid IP
+				pass
+			#print(f"File: {parsed_eml_files[x]}, IP:{first_hop_ip}")
+			bar()
+			x = x + 1
+	print(f"Done! {x} emails successfully parsed.")
 
-	# Write IP list to .xls file
-	workbook = xlsxwriter.Workbook(f"{cwd}\\ip_list\\ips.xlsx")
-	worksheet = workbook.add_worksheet()
+	print("Writing data to file... ")
+	df = pd.DataFrame.from_dict(parsed_eml_dict, orient='index', columns=['IP'])
+	file_name = f"{cwd}\\ip_list\\first_hop_addressess.xlsx"
+	df.to_excel(file_name)
+	print('Done!')
 
-	row = 0
-	column = 0
-
-	for item in ip_list:
-		worksheet.write(row, column, item)
-		row += 1
-
-	workbook.close()
+	with open(f'{cwd}\\becdig_dump\\dict_output.txt', 'w') as convert_file:
+		convert_file.write(json.dumps(parsed_eml_dict))
 
 if __name__ == '__main__':
     grab_email_ips()
--- becscan.py
+++ becscan.py
@@ -3,58 +3,49 @@ from bs4 import BeautifulSoup
 import ast
 from alive_progress import alive_bar
 import pandas as pd
+import json
+import os
 
 def grab_scamalytics_data():
-	# Load IPs
-	ip_list = []
+
+	cwd = os.getcwd()
+	with open(f'{cwd}\\becdig_dump\\dict_output.txt') as f:
+		data = f.read()
+
+	local_dict = json.loads(data)
+
 	rapid_lookup_data = {}
-	loc = "ip_list/ips.xlsx"
-	# Read row 2 and down from column 1
-	df = pd.read_excel(loc)
-	column_data = df.iloc[:, [0]]
-	
-	# Read row 1 from column 1
-	for column in column_data:
-		first_ip = column
 
-	# Convert dataframe items to list
-	remaining_ips = column_data.values.tolist()
+	key_list=list(local_dict.keys())
 
-	y = 0
-	# Add first ip to list
-	ip_list.append(first_ip)
-	
-	# Add remaining ips to list
-	for ips in remaining_ips:
-		ip = str(remaining_ips[y])
-		clean_ip = ip.replace('[','').replace(']','').replace('\'','')
-		ip_list.append(clean_ip)
-		y = y + 1
-		
 	# Scrape data
 	print("Grabbing Scamalytics data...for free!")
 	x = 0
-	with alive_bar(len(ip_list)) as bar:
-		for ip in ip_list:
+	with alive_bar(len(key_list)) as bar:
+		for key in key_list:
+			# Grab current email
+			email_name = key_list[x]
+			# Grab current IP address
+			ip_address = local_dict[email_name]
 			scrape_info = []
-			page = requests.get(f'https://scamalytics.com/ip/{ip_list[x]}')
+			page = requests.get(f'https://scamalytics.com/ip/{ip_address}')
 			soup = BeautifulSoup(page.text, 'html.parser')
 			scraped_page = soup.find_all('td')
 			for td in scraped_page:
 				scrape_info.append(td.text)
 			if scrape_info[14] == "Open":
-				rapid_lookup_data[ip_list[x]] = scrape_info[2], scrape_info[5], scrape_info[21], scrape_info[22], scrape_info[23], scrape_info[24], scrape_info[25], scrape_info[26]
+				rapid_lookup_data[ip_address] = email_name, scrape_info[2], scrape_info[5], scrape_info[21], scrape_info[22], scrape_info[23], scrape_info[24], scrape_info[25], scrape_info[26]
 			elif scrape_info[14] == "Closed":
-				rapid_lookup_data[ip_list[x]] = scrape_info[2], scrape_info[5], scrape_info[21], scrape_info[22], scrape_info[23], scrape_info[24], scrape_info[25], scrape_info[26]
+				rapid_lookup_data[ip_address] = email_name, scrape_info[2], scrape_info[5], scrape_info[21], scrape_info[22], scrape_info[23], scrape_info[24], scrape_info[25], scrape_info[26]
 			else:
-				rapid_lookup_data[ip_list[x]] = scrape_info[2], scrape_info[5], scrape_info[15], scrape_info[16], scrape_info[17], scrape_info[18], scrape_info[19], scrape_info[20]
+				rapid_lookup_data[ip_address] = email_name, scrape_info[2], scrape_info[5], scrape_info[15], scrape_info[16], scrape_info[17], scrape_info[18], scrape_info[19], scrape_info[20]
 			bar()
 			x = x + 1
 
 	# Write data
 	print("Writing data to file... ")
-	df = pd.DataFrame.from_dict(rapid_lookup_data, orient='index', columns=['ISP', 'Country', 'Anonymizing VPN', 'Tor Exit Node', 'Server', 'Public Proxy', 'Web Proxy', 'Search Engine Robot'])
-	file_name = "scamalytics_data.xlsx"
+	df = pd.DataFrame.from_dict(rapid_lookup_data, orient='index', columns=['Email', 'ISP', 'Country', 'Anonymizing VPN', 'Tor Exit Node', 'Server', 'Public Proxy', 'Web Proxy', 'Search Engine Robot'])
+	file_name = ('becdig_data.xlsx')
 	df.to_excel(file_name)
 	print('Done!')
 
